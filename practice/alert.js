93c84e8834979a3d55031e612ad706940d3627a3
Tomk0818 <tomomoka08@gmail.com>
2023-07-22 03:50:13 +0900

    [Add]課題の提出

---
+++ practice/alert.js
@@ -0,0 +1,11 @@
+function loadHello(){
+  alert("Hello!");
+  //Helloのダイアログが上から表示される。
+}
+function sum(){
+	let x = 3;
+	let y = 4;
+	let z = x + y;
+	alert(z);
+    }
+    //alertとして、各変数を足した数の７が表示される
